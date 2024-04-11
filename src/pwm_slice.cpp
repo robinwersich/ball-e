e62f02c4e7eec1f75aa1caa0a826712dee9b9d9d
Robin Wersich <wersich@ut.ee>
2024-04-11 12:19:26 +0300

    Improve PwmSlice handling of different frequencies

--- src/pwm_slice.cpp
+++ src/pwm_slice.cpp
@@ -1,5 +1,7 @@
 #include "pwm_slice.h"
 
+#include <cmath>
+
 #include "hardware/clocks.h"
 #include "hardware/pwm.h"
 #include "pico/stdlib.h"
@@ -16,10 +18,15 @@ std::shared_ptr<PwmSlice> PwmSlice::forPin(uint pin, uint frequency) {
 }
 
 void PwmSlice::set_frequency(uint frequency) {
-  uint32_t f_sys = clock_get_hz(clk_sys);  // typically 125'000'000 Hz
-  uint32_t f_clock = 1000000UL;
-  float divider = (float)f_sys / f_clock;
-  pwm_set_clkdiv(slice_num, divider);
-  period = f_clock / frequency;
+  const uint32_t f_sys = clock_get_hz(clk_sys);  // typically 125'000'000 Hz
+  period = f_sys / frequency;
+  if (period > 0xffff) {
+    // Period is too large to fit in 16bit int, need to reduce the frequency
+    const uint32_t f_target = frequency * 0xffff;
+    // make sure the clock frequency is low enough for the period to fit in 16bit int
+    uint8_t divider = std::ceil(f_sys / f_target);
+    pwm_set_clkdiv_int_frac(slice_num, divider, 0);
+    period = f_sys / divider / frequency;
+  }
   pwm_set_wrap(slice_num, period - 1);  // -1 because counter starts at 0
 }
