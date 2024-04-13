52a719427789d43c5b899b227ef88349a8c3fcfc
Robin Wersich <wersich@ut.ee>
2024-04-14 01:48:06 +0300

    Adapt to new circuit layout

--- src/main.cpp
+++ src/main.cpp
@@ -4,32 +4,47 @@
 
 const uint PWM_FREQUENCY = 25000;
 // -- motor 1 --
-const uint DIR1 = 4;
-const uint PWM1 = 5;
+const uint DIR1 = 0;
+const uint PWM1 = 1;
+const uint ENC1A = 2;
+const uint ENC1B = 3;
 // -- motor 2 --
-const uint DIR2 = 7;
-const uint PWM2 = 6;
+const uint ENC2B = 4;
+const uint ENC2A = 5;
+const uint DIR2 = 6;
+const uint PWM2 = 7;
 // -- motor 3 --
-const uint DIR3 = 12;
-const uint PWM3 = 13;
+const uint PWM3 = 8;
+const uint DIR3 = 9;
+const uint ENC3A = 10;
+const uint ENC3B = 11;
 
 int main() {
   std::array wheels = {
-    Omniwheel(210, std::make_unique<MotorDriverDRI0044>(PWM1, DIR1, PWM_FREQUENCY)),
-    Omniwheel(330, std::make_unique<MotorDriverDRI0044>(PWM2, DIR2, PWM_FREQUENCY)),
-    Omniwheel(90, std::make_unique<MotorDriverDRI0044>(PWM3, DIR3, PWM_FREQUENCY)),
+    Omniwheel(30, std::make_unique<MotorDriverDRI0044>(PWM1, DIR1, PWM_FREQUENCY), true),
+    Omniwheel(150, std::make_unique<MotorDriverDRI0044>(PWM2, DIR2, PWM_FREQUENCY)),
+    Omniwheel(270, std::make_unique<MotorDriverDRI0044>(PWM3, DIR3, PWM_FREQUENCY)),
   };
 
   Robot robot{std::move(wheels)};
-  float speed = 0.5;
 
   while (true) {
-    sleep_ms(500);
-    robot.drive(0, 0.5);
     sleep_ms(2000);
+    robot.drive(0, 0.7);
+    sleep_ms(1000);
     robot.stop();
     sleep_ms(500);
-    robot.drive(0, -1.0);
+    robot.drive(0.7, 0);
+    sleep_ms(1000);
+    robot.stop();
+    sleep_ms(500);
+    robot.drive(-0.7, -0.7);
+    sleep_ms(1000);
+    robot.stop();
+    sleep_ms(500);
+    robot.rotate(0.5);
+    sleep_ms(1000);
+    robot.rotate(-0.5);
     sleep_ms(1000);
     robot.stop();
   }
