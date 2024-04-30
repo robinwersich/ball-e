5f046d2e7521d97c5255284bcf5a941b4b067d4f
Robin Wersich <wersich@ut.ee>
2024-04-30 09:50:20 +0300

    Fix decoder direction issue

--- lib/motor_hardware/src/motor_decoder.cpp
+++ lib/motor_hardware/src/motor_decoder.cpp
@@ -3,11 +3,12 @@
 #include "pico/stdlib.h"
 
 MotorDecoder::MotorDecoder(uint8_t slot, bool swap_direction) : _slot{slot} {
+  _states[slot].swap_direction = swap_direction;
+
   const uint pin_a = 2 * slot;
   const uint pin_b = 2 * slot + 1;
   gpio_init(pin_a);
   gpio_set_dir(pin_a, GPIO_IN);
-
   gpio_init(pin_b);
   gpio_set_dir(pin_b, GPIO_IN);
 
@@ -28,14 +29,15 @@ std::array<MotorDecoderState, 8> MotorDecoder::_states{};
 void MotorDecoder::on_a_change(uint pin, uint32_t event) {
   const bool a_rising = event & GPIO_IRQ_EDGE_RISE;
   const uint8_t slot = pin / 2;
+  auto& state = _states[slot];
 
-  if (a_rising != gpio_get(pin + 1) xor _states[slot].swap_direction) {
-    ++_states[slot].count;
+  if (a_rising != gpio_get(pin + 1) xor state.swap_direction) {
+    ++state.count;
   } else {
-    --_states[slot].count;
+    --state.count;
   }
   if (a_rising) {
-    _states[slot].syncronized_count = _states[slot].count;
-    _states[slot].last_sync_micros = time_us_32();
+    state.syncronized_count = state.count;
+    state.last_sync_micros = time_us_32();
   }
 }
