05e7dadc68e9dc6b05f1a4c67e858ed559e1df91
Robin Wersich <wersich@ut.ee>
2024-05-26 23:14:53 +0300

    Fix oscillating velocity measurement at high polling frequencies

--- lib/motor_hardware/include/motor_state.h
+++ lib/motor_hardware/include/motor_state.h
@@ -12,8 +12,10 @@ class MotorState {
    * @param decoder_state The decoder state from which to read ticks.
    * @param ticks_per_revolution The number of ticks the encoder produces per motor revolution.
    * @param gear_ratio How many rotations of the motor are required for one shaft rotation.
+   * @param min_rpm The minimum RPM the motor can achieve. Based on this, the speed computation
+   *  will avoid returning a false zero speed at high polling rates when the motor is moving slowly.
    */
-  MotorState(const MotorDecoderState* decoder_state, float ticks_per_revolution, float gear_ratio);
+  MotorState(const MotorDecoderState* decoder_state, float ticks_per_revolution, float gear_ratio, float min_rpm = 0.0);
 
   /** Returns the current position in terms of revolutions since start of measurement. */
   double current_position_revs();
@@ -31,5 +33,7 @@ class MotorState {
   const MotorDecoderState* _decoder_state;
   uint32_t _last_count_micros = 0;
   int32_t _last_count = 0;
+  float _last_speed_tps = 0;
   float _revolutions_per_tick;  // shaft revolutions
+  uint32_t _max_sync_tick_interval;
 };
--- lib/motor_hardware/src/motor_state.cpp
+++ lib/motor_hardware/src/motor_state.cpp
@@ -1,12 +1,22 @@
 #include "motor_state.h"
 
-static const uint64_t max_measurement_interval = 1000000;
-static const float seconds_per_micro = 1.0 / 1000000;
+#include <limits>
+
+constexpr uint64_t max_measurement_interval = 1000000;
+constexpr float seconds_per_micro = 1.0 / 1000000;
 
 MotorState::MotorState(
-  const MotorDecoderState* decoder_state, float ticks_per_revolution, float gear_ratio
+  const MotorDecoderState* decoder_state, float ticks_per_revolution, float gear_ratio,
+  float min_rpm
 )
-  : _decoder_state{decoder_state}, _revolutions_per_tick{1 / (ticks_per_revolution * gear_ratio)} {}
+  : _decoder_state{decoder_state}
+  , _revolutions_per_tick{1 / (ticks_per_revolution * gear_ratio)}
+  , _max_sync_tick_interval{
+      min_rpm == 0
+        ? std::numeric_limits<uint32_t>::max()
+        // 1 / ((min_rpm/(60*1000000)) * (ticks_per_revolution/2) * gear_ratio)
+        : static_cast<uint32_t>(120000000 / (min_rpm * ticks_per_revolution * gear_ratio))
+    } {}
 
 double MotorState::current_position_revs() {
   return _decoder_state->count * static_cast<double>(_revolutions_per_tick);
@@ -16,15 +26,16 @@ double MotorState::current_position_deg() { return current_position_revs() * 360
 
 float MotorState::compute_speed_tps() {
   // use syncronized count to avoid timing variations
-  const auto new_count = _decoder_state->syncronized_count;
   const auto new_count_micros = _decoder_state->last_sync_micros;
-  const auto delta_count = new_count - _last_count;
   const auto delta_time = new_count_micros - _last_count_micros;
+  if (delta_time == 0) return _last_speed_tps;
+  const auto new_count = _decoder_state->syncronized_count;
+  const auto delta_count = new_count - _last_count;
+  if (delta_count == 0 and delta_time < _max_sync_tick_interval) return _last_speed_tps;
   _last_count = new_count;
   _last_count_micros = new_count_micros;
-
-  if (delta_time == 0 || delta_time > max_measurement_interval) return 0;
-  return delta_count / (delta_time * seconds_per_micro);
+  if (delta_time > max_measurement_interval) return _last_speed_tps = 0;
+  return _last_speed_tps = delta_count / (delta_time * seconds_per_micro);
 }
 
 float MotorState::compute_speed_rps() { return compute_speed_tps() * _revolutions_per_tick; }
